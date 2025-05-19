dca6947e5a93e7d5c21020e6c31b7c35845b4b16
Laerte Quinui <laertequinuio@gmail.com>
2025-05-19 14:53:19 -0300

    🎨 style: Override Radix default font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import "./theme-config.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
