6d4d02ad320d134c1a657e25a367d22e40d3b42d
Laerte Quinui <laertequinuio@gmail.com>
2025-05-20 11:58:51 -0300

    ♻️refactor: Move the IssuesTable component to a separate file

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,44 +1,14 @@
-import { prisma } from "@/prisma/client"
-import { Table } from "@radix-ui/themes"
-import IssueStatusBadge from "./IssueStatusBadge"
+import IssuesTable from "./IssuesTable"
 import NewIssueButton from "./NewIssueButton"
 
 const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany()
-
   return (
     <div>
       <div className="flex mb-4">
         <NewIssueButton />
       </div>
 
-      <Table.Root variant='surface'>
-        <Table.Header>
-          <Table.Row>
-            <Table.Cell>Issue</Table.Cell>
-            <Table.Cell className='hidden md:table-cell'>Status</Table.Cell>
-            <Table.Cell className='hidden md:table-cell'>Created at</Table.Cell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                {issue.title}
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssuesTable />
     </div>
   )
 }
