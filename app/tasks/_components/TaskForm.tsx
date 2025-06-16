7b90e276f56ca59c7ca1b2a9ae563000ca02fc05
Laerte Quinui <laertequinuio@gmail.com>
2025-06-16 17:14:20 -0300

    🎨style: Adjust layout in the task form

--- app/tasks/_components/TaskForm.tsx
+++ app/tasks/_components/TaskForm.tsx
@@ -4,7 +4,7 @@ import ErrorMessage from '@/app/components/ErrorMessage'
 import { Task } from '@/app/generated/prisma'
 import { patchTaskSchema } from '@/app/validationSchemas'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Flex, TextField } from '@radix-ui/themes'
+import { Button, Flex, Grid, Heading, Text, TextField } from '@radix-ui/themes'
 import axios from 'axios'
 import 'easymde/dist/easymde.min.css'
 import dynamic from 'next/dynamic'
@@ -47,47 +47,72 @@ const TaskForm = ({ task }: { task?: Task }) => {
   })
 
   return (
-    <form className="max-w-xl space-y-4" onSubmit={onSubmit}>
-      <TextField.Root
-        placeholder="Title"
-        defaultValue={task?.title}
-        {...register('title')}
-      />
-      <ErrorMessage>{errors.title?.message}</ErrorMessage>
-
-      <Controller
-        name="description"
-        control={control}
-        defaultValue={task?.description}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <ErrorMessage>{errors.description?.message}</ErrorMessage>
-
-      <Flex gap="4">
-        <Controller
-          name="status"
-          control={control}
-          defaultValue={task?.status || 'TO_DO'}
-          render={({ field }) => <SelectStatus {...field} />}
-        />
-
-        <Flex direction="column">
-          <Controller
-            name="deadline"
-            control={control}
-            defaultValue={task?.deadline || new Date()}
-            render={({ field }) => <DatePicker {...field} clearBtn={false} />}
+    <>
+      <Heading className="text-stone-900">
+        {task ? 'Edit task' : 'New task'}
+      </Heading>
+      <form className="max-w-xl space-y-4 mt-6" onSubmit={onSubmit}>
+        <Flex direction="column" gap="1">
+          <Text as="label" className="text-stone-500 font-medium">
+            Title
+          </Text>
+          <TextField.Root
+            placeholder="Do scrumbled eggs..."
+            defaultValue={task?.title}
+            {...register('title')}
           />
-          <ErrorMessage>{errors.deadline?.message}</ErrorMessage>
+          <ErrorMessage>{errors.title?.message}</ErrorMessage>
         </Flex>
-      </Flex>
 
-      <Button disabled={isSubmitting} loading={isSubmitting} className="">
-        {!task ? 'Create new task' : 'Save changes'}
-      </Button>
-    </form>
+        <Flex direction="column" gap="1">
+          <Text as="label" className="text-stone-500 font-medium">
+            Description
+          </Text>
+          <Controller
+            name="description"
+            control={control}
+            defaultValue={task?.description}
+            render={({ field }) => (
+              <SimpleMDE
+                placeholder="To do: buy eggs, buy salt, buy pan..."
+                {...field}
+              />
+            )}
+          />
+          <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        </Flex>
+
+        <Grid columns="2" gap="4">
+          <Flex direction="column" gap="1">
+            <Text as="label" className="text-stone-500 font-medium">
+              Status
+            </Text>
+            <Controller
+              name="status"
+              control={control}
+              defaultValue={task?.status || 'TO_DO'}
+              render={({ field }) => <SelectStatus {...field} />}
+            />
+          </Flex>
+
+          <Flex direction="column" gap="1">
+            <Text as="label" className="text-stone-500 font-medium">
+              Deadline
+            </Text>
+            <Controller
+              name="deadline"
+              control={control}
+              defaultValue={task?.deadline || new Date()}
+              render={({ field }) => <DatePicker {...field} clearBtn={false} />}
+            />
+          </Flex>
+        </Grid>
+
+        <Button disabled={isSubmitting} loading={isSubmitting} className="">
+          {!task ? 'Create new task' : 'Save changes'}
+        </Button>
+      </form>
+    </>
   )
 }
 
