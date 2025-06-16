f7cd727f306c455863cb1f0e4eecad6a410366d1
Laerte Quinui <laertequinuio@gmail.com>
2025-06-16 12:38:17 -0300

    ✨feat(task-form): Add a select status

---
+++ app/tasks/_components/SelectStatus.tsx
@@ -0,0 +1,61 @@
+import { TaskStatus } from '@/app/generated/prisma'
+import {
+  CheckmarkCircle02Icon,
+  DashedLineCircleIcon,
+  Progress01Icon,
+} from '@hugeicons/core-free-icons'
+import { HugeiconsIcon, IconSvgElement } from '@hugeicons/react'
+import { Flex, Select } from '@radix-ui/themes'
+
+const SelectStatus = ({ ...field }) => {
+  const statuses: {
+    label: string
+    value: TaskStatus
+    className: string
+    icon: IconSvgElement
+  }[] = [
+    {
+      label: 'To do',
+      value: 'TO_DO',
+      className: 'hover:!bg-orange-100',
+      icon: DashedLineCircleIcon,
+    },
+    {
+      label: 'Doing',
+      value: 'DOING',
+      className: 'hover:!bg-indigo-100',
+      icon: Progress01Icon,
+    },
+    {
+      label: 'Done',
+      value: 'DONE',
+      className: 'hover:!bg-green-100',
+      icon: CheckmarkCircle02Icon,
+    },
+  ]
+
+  return (
+    <Select.Root
+      defaultValue={field.value ?? 'TO_DO'}
+      onValueChange={field.onChange}
+    >
+      <Select.Trigger />
+      <Select.Content ref={field.ref} variant="soft">
+        {statuses.map((status) => (
+          <Select.Item
+            key={status.value}
+            value={status.value}
+            className={status.className}
+          >
+            <Flex align="center" gap="2">
+              <HugeiconsIcon icon={status.icon} size={16} />
+              {status.label}
+            </Flex>
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  )
+}
+
+export default SelectStatus
--- app/tasks/_components/TaskForm.tsx
+++ app/tasks/_components/TaskForm.tsx
@@ -1,25 +1,33 @@
 'use client'
-import ErrorMessage from "@/app/components/ErrorMessage";
-import { Task } from "@/app/generated/prisma";
-import { TaskSchema } from "@/app/validationSchemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, TextField } from "@radix-ui/themes";
-import axios from "axios";
-import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { z } from "zod";
+import ErrorMessage from '@/app/components/ErrorMessage'
+import { Task } from '@/app/generated/prisma'
+import { patchTaskSchema } from '@/app/validationSchemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Flex, TextField } from '@radix-ui/themes'
+import axios from 'axios'
+import 'easymde/dist/easymde.min.css'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import SelectStatus from './SelectStatus'
 
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+})
 
-type TaskFormData = z.infer<typeof TaskSchema>
+type TaskFormData = z.infer<typeof patchTaskSchema>
 
 const TaskForm = ({ task }: { task?: Task }) => {
   const router = useRouter()
-  const { register, control, handleSubmit, formState: { errors } } = useForm<TaskFormData>({
-    resolver: zodResolver(TaskSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TaskFormData>({
+    resolver: zodResolver(patchTaskSchema),
   })
 
   const [isSubmitting, setSubmitting] = useState(false)
@@ -27,21 +35,18 @@ const TaskForm = ({ task }: { task?: Task }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true)
-      if(task) await axios.patch('/api/tasks/' + task.id, data)
+      if (task) await axios.patch('/api/tasks/' + task.id, data)
       else await axios.post('/api/tasks', data)
       router.push('/tasks')
       router.refresh()
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (err) {
       setSubmitting(false)
     }
   })
 
   return (
-    <form
-      className="max-w-xl space-y-4"
-      onSubmit={onSubmit}
-    >
+    <form className="max-w-xl space-y-4" onSubmit={onSubmit}>
       <TextField.Root
         placeholder="Title"
         defaultValue={task?.title}
@@ -53,16 +58,22 @@ const TaskForm = ({ task }: { task?: Task }) => {
         name="description"
         control={control}
         defaultValue={task?.description}
-        render={
-          ({field})=> <SimpleMDE placeholder="Description" {...field} />
-        }
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
       />
       <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-      <Button
-        disabled={isSubmitting}
-        loading={isSubmitting}
-      >
+      <Flex>
+        <Controller
+          name="status"
+          control={control}
+          defaultValue={task?.status}
+          render={({ field }) => <SelectStatus {...field} />}
+        />
+      </Flex>
+
+      <Button disabled={isSubmitting} loading={isSubmitting}>
         {!task ? 'Create new task' : 'Save changes'}
       </Button>
     </form>
