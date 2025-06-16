fbb885fda0d99c253d39cd3d7e0b11d1273a0249
Laerte Quinui <laertequinuio@gmail.com>
2025-06-16 16:45:26 -0300

    🎨style: Adjust layout for task details

--- app/tasks/TaskStatusBadge.tsx
+++ app/tasks/TaskStatusBadge.tsx
@@ -1,18 +1,31 @@
+import { HugeiconsIcon, IconSvgElement } from '@hugeicons/react'
 import { Badge } from '@radix-ui/themes'
+import { Responsive } from '@radix-ui/themes/props'
 import { TaskStatus } from '../generated/prisma'
 
 const statusMap: Record<
   TaskStatus,
-  { label: string, color: 'orange' | 'indigo' | 'green' }
+  { label: string; color: 'orange' | 'indigo' | 'green' }
 > = {
   TO_DO: { label: 'To do', color: 'orange' },
   DOING: { label: 'Doing', color: 'indigo' },
-  DONE: { label: 'Done', color: 'green' }
+  DONE: { label: 'Done', color: 'green' },
 }
 
-const TaskStatusBadge = ({ status }: { status: TaskStatus }) => {
+const TaskStatusBadge = ({
+  status,
+  size,
+  icon,
+}: {
+  status: TaskStatus
+  size?: Responsive<'1' | '2' | '3'>
+  icon?: IconSvgElement
+}) => {
   return (
-    <Badge color={statusMap[status].color} >{statusMap[status].label}</Badge>
+    <Badge color={statusMap[status].color} className="w-fit" size={size || '1'}>
+      {icon && <HugeiconsIcon icon={icon} size={12} />}
+      {statusMap[status].label}
+    </Badge>
   )
 }
 
--- app/tasks/[id]/TaskDetails.tsx
+++ app/tasks/[id]/TaskDetails.tsx
@@ -1,26 +1,76 @@
 import { Task } from '@/app/generated/prisma'
-import { Flex, Heading, Separator, Text } from '@radix-ui/themes'
+import {
+  AlarmClockIcon,
+  Calendar03Icon,
+  CheckmarkCircle02Icon,
+  DashedLineCircleIcon,
+  Progress01Icon,
+  Tag01Icon,
+} from '@hugeicons/core-free-icons'
+import { HugeiconsIcon, IconSvgElement } from '@hugeicons/react'
+import { Box, Flex, Grid, Heading, Separator, Text } from '@radix-ui/themes'
 import MarkdownRender from 'react-markdown'
 import TaskStatusBadge from '../TaskStatusBadge'
 
 const TaskDetails = ({ task }: { task: Task }) => {
+  const statusIcon = {
+    TO_DO: DashedLineCircleIcon,
+    DOING: Progress01Icon,
+    DONE: CheckmarkCircle02Icon,
+  }
+
   return (
     <>
       <Heading>{task.title}</Heading>
-      <Flex gap='4' pt='2'>
-        <TaskStatusBadge status={task.status} />
-        <Text className='text-stone-500'>{task.createdAt.toDateString()}</Text>
-      </Flex>
+      <Grid columns="6" gapY="2" gapX="4" mt="4">
+        <DetailLabel label="Status" icon={Tag01Icon} />
+        <Box className="col-span-5">
+          <TaskStatusBadge
+            status={task.status}
+            size="2"
+            icon={statusIcon[task.status]}
+          />
+        </Box>
 
-      <Separator my='6' size='4' />
+        <DetailLabel label="Created at" icon={Calendar03Icon} />
+        <Text className="text-stone-500 col-span-5">
+          {task.createdAt.toDateString()}
+        </Text>
 
-      <Text as='p' size='2' weight='bold' mb='2' className='text-stone-400'>Description</Text>
-      <Text as='div' className='prose prose-stone max-w-full'>
+        <DetailLabel label="Deadline" icon={AlarmClockIcon} />
+        <Text className="text-stone-500 col-span-5">
+          {task.deadline.toDateString()}
+        </Text>
+      </Grid>
+
+      <Separator my="6" size="4" />
+
+      <Text as="p" size="2" weight="bold" mb="2" className="text-stone-400">
+        Description
+      </Text>
+      <Text as="div" className="prose prose-stone max-w-full">
         <MarkdownRender>{task.description}</MarkdownRender>
       </Text>
 
-      <Separator my='6' size='4' />
+      <Separator my="6" size="4" />
     </>
+  )
+}
+
+const DetailLabel = ({
+  label,
+  icon,
+}: {
+  label: string
+  icon: IconSvgElement
+}) => {
+  return (
+    <Flex align="center" gap="2">
+      <HugeiconsIcon size={16} icon={icon} className="text-stone-400" />
+      <Text size="2" className="text-stone-400">
+        {label}
+      </Text>
+    </Flex>
   )
 }
 
