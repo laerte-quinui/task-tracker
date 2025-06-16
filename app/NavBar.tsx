82fe60b537ee23da4eaf36bd3e6c235b129c949e
Laerte Quinui <laertequinuio@gmail.com>
2025-06-16 17:28:28 -0300

    🎨style: Change app icon in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,25 +1,34 @@
 'use client'
 
-import { Bug02Icon, LogoutSquare02Icon } from '@hugeicons/core-free-icons'
+import {
+  LogoutSquare02Icon,
+  StickyNote02Icon,
+} from '@hugeicons/core-free-icons'
 import { HugeiconsIcon } from '@hugeicons/react'
-import { Avatar, Container, DropdownMenu, Flex, Skeleton, Text } from '@radix-ui/themes'
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Skeleton,
+  Text,
+} from '@radix-ui/themes'
 import classNames from 'classnames'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const NavBar = () => {
-
   return (
-    <nav className='py-2 px-4 rounded-lg bg-stone-900 m-2 mb-8'>
+    <nav className="py-2 px-4 rounded-lg bg-stone-900 m-2 mb-8">
       <Container>
-        <Flex align='center' justify='between'>
-          <Flex align='center' gap='6'>
-            <Link href='/'>
-              <HugeiconsIcon
-                icon={Bug02Icon}
-                className='size-8 p-1 rounded-lg text-stone-50 hover:bg-stone-50 hover:text-stone-900 transition-colors'
-              />
+        <Flex align="center" justify="between">
+          <Flex align="center" gap="6">
+            <Link href="/">
+              <Box className="flex items-center justify-center p-1 rounded-lg text-stone-50 hover:bg-stone-50 hover:text-stone-900 transition-colors">
+                <HugeiconsIcon icon={StickyNote02Icon} size={24} />
+              </Box>
             </Link>
             <NavLinks />
           </Flex>
@@ -34,19 +43,19 @@ const NavBar = () => {
 const NavLinks = () => {
   const currentPath = usePathname()
   const links = [
-    { label: 'Tasks', href: '/tasks' },
     { label: 'Dashboard', href: '/' },
+    { label: 'Tasks', href: '/tasks' },
   ]
 
   return (
-    <ul className='flex gap-2'>
+    <ul className="flex gap-2">
       {links.map((link) => (
         <li
           key={link.label}
-          className={classNames(
-            'nav-link',
-            { '!text-stone-900 bg-stone-50 hover:!bg-stone-200': currentPath === link.href },
-          )}
+          className={classNames('nav-link', {
+            '!text-stone-900 bg-stone-50 hover:!bg-stone-200':
+              currentPath === link.href,
+          })}
         >
           <Link href={link.href}>{link.label}</Link>
         </li>
@@ -62,8 +71,8 @@ const AuthStatus = () => {
   if (status === 'unauthenticated')
     return (
       <Link
-        className='nav-link border border-stone-700'
-        href='/api/auth/signin'
+        className="nav-link border border-stone-700"
+        href="/api/auth/signin"
       >
         Log in
       </Link>
@@ -75,25 +84,27 @@ const AuthStatus = () => {
         <Avatar
           src={session!.user!.image!}
           fallback={session!.user!.name![0]}
-          size='4'
-          radius='full'
+          size="4"
+          radius="full"
           // loading='lazy'
-          variant='solid'
-          referrerPolicy='no-referrer'
-          className='p-1 cursor-pointer hover:bg-stone-800 transition-all'
+          variant="solid"
+          referrerPolicy="no-referrer"
+          className="p-1 cursor-pointer hover:bg-stone-800 transition-all"
         />
       </DropdownMenu.Trigger>
-      <DropdownMenu.Content variant='soft' color='gray'>
+      <DropdownMenu.Content variant="soft" color="gray">
         <DropdownMenu.Label>
           <Text>{session!.user!.email}</Text>
         </DropdownMenu.Label>
-        <DropdownMenu.Item color='red'>
+        <DropdownMenu.Item color="red">
           <HugeiconsIcon
             icon={LogoutSquare02Icon}
-            size='18'
-            className='pointer-events-none'
+            size="18"
+            className="pointer-events-none"
           />
-          <Link href='/api/auth/signout' className='w-full'>Sign out</Link>
+          <Link href="/api/auth/signout" className="w-full">
+            Sign out
+          </Link>
         </DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
