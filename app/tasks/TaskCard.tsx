6c34193aaa42aa5ebf98d087068713e1429728c0
Laerte Quinui <laertequinuio@gmail.com>
2025-06-18 15:35:07 -0300

    ✨feat: Add redirect link to task card

--- app/tasks/TaskCard.tsx
+++ app/tasks/TaskCard.tsx
@@ -1,29 +1,30 @@
 import { AlarmClockIcon } from '@hugeicons/core-free-icons'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { Badge, Card, Text } from '@radix-ui/themes'
-import { TaskStatus } from '../generated/prisma'
+import Link from 'next/link'
 
 export interface TaskCardProps {
   id: number
   title: string
   description: string
-  status: TaskStatus
   deadline: Date
 }
 
-const TaskCard = ({ title, description, deadline }: TaskCardProps) => {
+const TaskCard = ({ id, title, description, deadline }: TaskCardProps) => {
   return (
-    <Card className="cursor-grab w-full hover:bg-stone-100 duration-150">
-      <Text className="text-stone-800 font-medium">{title}</Text>
-      <Text as="p" size="2" className="text-stone-500">
-        {description}
-      </Text>
+    <Link href={`/tasks/${id}`}>
+      <Card className="cursor-grab w-full hover:bg-stone-100 duration-150">
+        <Text className="text-stone-800 font-medium">{title}</Text>
+        <Text as="p" size="2" className="text-stone-500">
+          {description}
+        </Text>
 
-      <Badge color="gray" mt="4">
-        <HugeiconsIcon icon={AlarmClockIcon} size={12} />
-        <Text size="1">{deadline.toDateString()}</Text>
-      </Badge>
-    </Card>
+        <Badge color="gray" mt="4">
+          <HugeiconsIcon icon={AlarmClockIcon} size={12} />
+          <Text size="1">{deadline.toDateString()}</Text>
+        </Badge>
+      </Card>
+    </Link>
   )
 }
 
